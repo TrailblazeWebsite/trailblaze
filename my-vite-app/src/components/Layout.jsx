53241e5ad6d60b4b6951334b002d5b1012a43999
Silas Meier <silas.meier@students.fhnw.ch>
2025-08-07 18:46:31 +0200

    add authentication

--- my-vite-app/src/components/Layout.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../styles/App.css"; // damit die container-Klasse verfügbar ist
-
-const Layout = ({ children }) => {
-    return <div className="container">{children}</div>;
-};
-
-export default Layout;
